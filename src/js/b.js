2617dc0c953c660a05bca73fe3f1d30d0eecaf75
chemdemo <yangdemo@gmail.com>
2016-03-11 10:34:20 +0800

    clean scripts in templates, webpack can suto inject scripts

--- src/js/b.js
+++ src/js/b.js
@@ -7,15 +7,15 @@
 
 'use strict';
 
-console.info('require page b.');
+console.info('require page b.')
 
-require('commonCss');
-require('../css/b.css');
+require('commonCss')
+require('../css/b.css')
 
-require('zepto');
+require('zepto')
 
 // 直接使用npm模块
-var _ = require('lodash');
+const _ = require('lodash')
 
-var report = require('./helpers/report');
-var bar = require('./helpers/bar');
+const report = require('./helpers/report')
+const bar = require('./helpers/bar')
