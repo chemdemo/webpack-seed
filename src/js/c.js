e191c638ee587335abb5643fb32999ce3290bfb6
chemdemo <yangdemo@gmail.com>
2016-03-28 10:34:44 +0800

    modify demo

--- src/js/c.js
+++ src/js/c.js
@@ -2,16 +2,17 @@
 * @Author: dmyang
 * @Date:   2015-08-31 21:17:45
 * @Last Modified by:   dmyang
-* @Last Modified time: 2015-09-14 14:54:37
+* @Last Modified time: 2016-03-28 09:48:22
 */
 
 'use strict';
 
 console.info('require page c.');
 
-require('commonCss');
+import 'commonCss'
+import '../scss/c.scss'
 
-require('zepto');
+let $ = require('zepto')
 
 // 直接使用npm模块
 var _ = require('lodash');
@@ -19,3 +20,5 @@ var _ = require('lodash');
 var report = require('./helpers/report');
 var bar = require('./helpers/bar');
 var url = require('./utils/url');
+
+console.log(_)
